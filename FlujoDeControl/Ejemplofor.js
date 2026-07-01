261ff4189c16b06fd2c5cc544614f8570bd0abb5
Dannis Dayana Rincon Guevara <dannis.rincon1997@gmail.com>
2026-06-30 19:57:01 -0500

    Agregué las carpetas

---
+++ FlujoDeControl/Ejemplofor.js
@@ -0,0 +1,23 @@
+
+
+let carrito = ["Arroz", "Pollo", "Gaseosa"];
+for (let i = 0; i < carrito.length; i++) {
+  console.log("Producto: " + carrito[i]);
+}
+
+
+let carrito = [
+  { nombre: "Arroz", precio: 8000 },
+  { nombre: "Pollo", precio: 25000 },
+  { nombre: "Gaseosa", precio: 6000 }
+];
+
+ 
+
+for (let i = 0; i < carrito.length; i++) {
+  if (carrito[i].precio > 20000) {
+    console.log(carrito[i].nombre + " es un producto caro ");
+  } else {
+    console.log(carrito[i].nombre + " tiene buen precio ");
+  }
+}
